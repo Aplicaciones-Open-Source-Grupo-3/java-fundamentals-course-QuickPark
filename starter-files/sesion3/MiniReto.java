2d8dc76d9765fe403602e5797ce4fb2878abde0f
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 17:32:35 -0500

    docs: add Mini Reto
    Initial setup for MiniReto class with main method and comments for implementation steps.

---
+++ starter-files/sesion3/MiniReto.java
@@ -0,0 +1,17 @@
+// PASO 1: Crea la clase y declara las notas
+
+  public class Main {
+       public static void main(String[] args) {
+    
+//PASO 2: Calcula el promedio
+          
+//PASO 3: Usa if para ver si está aprobado / desaprobado
+
+//PASO 4: Usa ELSE IF para mostrar la categoría
+         
+//PASO 5: Usa FOR para mostrar las notas registradas
+  }
+    
+}
+
+
