79749f3eae0ab3eef58871213a860d4b2c229c88
PengwinKingdom <kai.supernova097@gmail.com>
2025-11-14 07:38:05 -0500

    Create ConversorCelsiusFahrenheit.java

---
+++ starter-files/sesion2/ConversorCelsiusFahrenheit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // TODO: pedir dos números al usuario
+        // TODO: conversión de Celsius a Fahrenheit
+        // TODO: mostrar los resultados
+
+        sc.close();
+    }
+}
