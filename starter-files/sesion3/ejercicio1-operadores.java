45902af5672f888343680b8e62d3120d2cc1bf53
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 17:11:53 -0500

    docs: add example1

---
+++ starter-files/sesion3/ejercicio1-operadores.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+       //TODO: declara las variables
+       //TODO: realiza la operacion para calcular el total
+       //TODO: Muestra el resultado
+    }
+}
+
