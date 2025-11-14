75c98fbb244672e2e88b6d887044374ab77af24b
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 17:28:56 -0500

    docs: add ejercicio 4

---
+++ starter-files/sesion3/Ejercicio4-Bucles.java
@@ -0,0 +1,14 @@
+//BUCLES
+public class Main {
+    public static void main(String[] args) {
+
+        // Usa for para contar del 1 al 5
+
+        // Usa while para hacer la cuenta regresiva
+        int n = 3;
+       
+        // Usa do-while para que se ejecute al menos una vez
+        int x = 1;
+      
+    }
+}
