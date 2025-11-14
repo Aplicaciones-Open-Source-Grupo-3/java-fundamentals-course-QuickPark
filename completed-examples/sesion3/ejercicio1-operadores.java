c4ea6d3b11fb9b69656bdd7ab40c4c9d9f9e4195
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 16:57:26 -0500

    docs: add example

---
+++ completed-examples/sesion3/ejercicio1-operadores.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+       
+       int precio=20;
+       int cantidad=3;
+       int total= precio*cantidad;
+        System.out.println("Total a pagar:" + total);
+    }
+}
